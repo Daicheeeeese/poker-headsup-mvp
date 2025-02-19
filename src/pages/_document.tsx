2a297802756296a8ee2abb22771de822898cbf65
Daicheeeeese <daicheese7@gmail.com>
2025-02-19 17:35:10 +0900

    add: Google AdSense verification meta tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
+        {/* Google AdSense 確認用のメタタグ */}
+        <meta name="google-adsense-account" content="ca-pub-8437153362194493" />
+        
         <script
           async
           src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_ID}`}
